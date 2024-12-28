27fa2bfebfad47984aa475f437f86da873123521
LakshayaShona <shonasonuabc@gmail.com>
2024-12-28 11:15:35 +0530

    JavaScript

---
+++ javascript/declaration.js
@@ -0,0 +1,10 @@
+a = 10
+console.log(a)
+b = "AIML, CSE, IT"
+console.log(b)
+const c = "https://kongu.ac.in"
+console.log(c)
+var d = "HI"
+console.log(d)
+let e = 20
+console.log(e)
---
+++ javascript/operators.js
@@ -0,0 +1,49 @@
+//Assignment operator
+a = 10
+console.log(a)
+
+//Arithmetic operator
+b = 20
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+//Unary Operator
+a++
+b--
+console.log(a)
+console.log(b)
+
+//Relational operator
+console.log(a<b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a>=b)
+console.log(a==b)
+console.log(a===b)
+console.log(a!=b)
+console.log(a!==b)
+
+//Logical operator
+if(a == 11 && b == 19){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+if(a == 9 || b == 19){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+console.log(!a)
+
+//bitwise operator
+console.log(a&b)
+console.log(a|b)
+console.log(a^b)
+console.log(a>>2)
+console.log(a<<2)
