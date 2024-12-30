2fa3039528011a851aa7a0197c12a38bafbe4169
LakshayaShona <shonasonuabc@gmail.com>
2024-12-30 09:21:59 +0530

    First Commit of react

---
+++ react_example1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ClassCompEg from './Components/classComponents/clscompeg.jsx';
+import Def_component from './Components/functionalComponent/funcompeg.jsx';
+function App() {
+  return (
+    <div>
+      <ClassCompEg/>
+      <Def_component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_example1/src/Components/functionalComponent/funcompeg.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function funcompeg() {
+  return (
+    <div>
+      <h1>Work of Default:</h1>
+      <h2>Default is used to export only one component</h2>
+    </div>
+  )
+}
